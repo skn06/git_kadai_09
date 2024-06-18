96a5a276f0fde20a513b68c4f5f0aa0ace84f5d8
skn06 <skn.y360@gmail.com>
2024-06-18 20:26:12 +0900

    add judgement kadai_09.py

--- kadai_09.py
+++ kadai_09.py
@@ -12,5 +12,13 @@ while times < max_times:
     # ユーザーのキーボード入力を受け取る
     input_number = input("Enter your guess: ")
 
-    # 回答回数をカウント
+    # 回答回数をカウントする
     times += 1
+
+    # 正解かどうかを判定
+    if input_number < correct_number:
+        print("Too low! Try again.")
+    elif input_number > correct_number:
+        print("Too high! Try again.")
+    else:
+        print("Congratulations! Your number is correct.")
